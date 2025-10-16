405538ec578ce04cd8a9a2be9037f01b12cdc504
Rasoloson Henintsoa <har@esokia-webagency.com>
2025-10-16 17:30:41 +0300

    update vercel config with proxy

---
+++ src/api/hero-detail.ts
@@ -0,0 +1,15 @@
+import type { NowRequest, NowResponse } from "@vercel/node";
+import fetch from "node-fetch";
+
+export default async (req: NowRequest, res: NowResponse) => {
+  try {
+    const response = await fetch(
+      "https://mlbb-stats.ridwaanhall.com/hero-list/"
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching hero list:", error);
+    res.status(500).json({ error: "Failed to fetch hero list" });
+  }
+};
