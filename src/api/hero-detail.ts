6a2cd3fd6dbb799d9a12b8f0ee1e9b3b49031a76
Rasoloson Henintsoa <har@esokia-webagency.com>
2025-10-16 17:42:08 +0300

    update vercel proxy config

--- src/api/hero-detail.ts
+++ src/api/hero-detail.ts
@@ -1,15 +1,17 @@
-import type { NowResponse } from "@vercel/node";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import fetch from "node-fetch";
 
-export default async (res: NowResponse) => {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const { id } = req.query;
   try {
     const response = await fetch(
-      "https://mlbb-stats.ridwaanhall.com/hero-list/"
+      `https://mlbb-stats.ridwaanhall.com/api/hero-detail/${id}`
     );
     const data = await response.json();
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(data);
   } catch (error) {
-    console.error("Error fetching hero list:", error);
-    res.status(500).json({ error: "Failed to fetch hero list" });
+    console.error("Error fetching hero detail:", error);
+    res.status(500).json({ error: "Failed to fetch hero detail" });
   }
-};
+}
--- src/api/hero-list.ts
+++ src/api/hero-list.ts
@@ -1,15 +1,16 @@
-import type { NowResponse } from "@vercel/node";
+import type { VercelResponse } from "@vercel/node";
 import fetch from "node-fetch";
 
-export default async (res: NowResponse) => {
+export default async function handler(res: VercelResponse) {
   try {
     const response = await fetch(
-      "https://mlbb-stats.ridwaanhall.com/hero-list/"
+      "https://mlbb-stats.ridwaanhall.com/api/hero-list/"
     );
     const data = await response.json();
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(data);
   } catch (error) {
     console.error("Error fetching hero list:", error);
     res.status(500).json({ error: "Failed to fetch hero list" });
   }
-};
+}
